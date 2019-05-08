0b9b00ae2138e7949a84f16617027ef0b07f89a1
Renan de Paula <renan.nuclearis@gmail.com>
2019-05-08 18:32:06 -0300

    Melhorias na tela de cadastro

--- app/src/main/java/br/edu/ucsal/colabmeiapp/activity/CadastroActivity.java
+++ app/src/main/java/br/edu/ucsal/colabmeiapp/activity/CadastroActivity.java
@@ -1,11 +1,14 @@
 package br.edu.ucsal.colabmeiapp.activity;
 
 import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -15,30 +18,78 @@ import br.edu.ucsal.colabmeiapp.R;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private TextInputEditText campoNome, campoRazao, campoCPF, campoCNPJ, campoTelefone, campoEndereco, campoEmail, campoSenha, campoConfirmaSenha;
+    private Switch switchTipoUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         getSupportActionBar().hide();
 
-        final TextInputEditText campoCPFeCNPJ = findViewById(R.id.cadastro_TextCPF);
-        Switch switchCadastro = findViewById(R.id.cadastro_switch);
+        //Inicializa componentes
+        campoNome = findViewById(R.id.cadastro_TextNome);
+        campoRazao = findViewById(R.id.cadastro_TextRazaoSocial);
+        campoCPF = findViewById(R.id.cadastro_TextCPF);
+        campoCNPJ = findViewById(R.id.cadastro_TextCNPJ);
+        campoTelefone = findViewById(R.id.cadastro_TextTelefone);
+        campoEndereco = findViewById(R.id.cadastro_TextEndereco);
+        campoEmail = findViewById(R.id.cadastro_TextEmail);
+        campoSenha = findViewById(R.id.cadastro_TextSenha);
+        campoConfirmaSenha = findViewById(R.id.cadastro_TextConfirmaSenha);
+        switchTipoUsuario = findViewById(R.id.cadastro_switch);
 
-        // Guardando o texto original do Hint
-        final CharSequence hintPadrao =  campoCPFeCNPJ.getHint();
+        final TextInputLayout layoutNome = findViewById(R.id.textInputLayoutNome);
+        final TextInputLayout layoutRazao = findViewById(R.id.textInputLayoutRazao);
+        final TextInputLayout layoutCPF = findViewById(R.id.textInputLayoutCPF);
+        final TextInputLayout layoutCNPJ = findViewById(R.id.textInputLayoutCNPJ);
 
-        switchCadastro.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        switchTipoUsuario.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
-                // Verificando se o switch está marcado, caso esteja, alterar o valor para 'CNPJ', caso não, restaurar para o valor padrão "CPF"
+                // Verificando se o switch de pessoa juridica está marcado, caso esteja, exibir os elementos 'CNPJ' e 'Razao Social' e ocultar 'Nome" e 'CPF'
                 if (checked) {
-                    campoCPFeCNPJ.setHint("CNPJ");
+                    layoutNome.setVisibility(View.GONE);
+                    layoutRazao.setVisibility(View.VISIBLE);
+                    layoutCPF.setVisibility(View.GONE);
+                    layoutCNPJ.setVisibility(View.VISIBLE);
                 } else {
-                    campoCPFeCNPJ.setHint(hintPadrao);
+                    layoutNome.setVisibility(View.VISIBLE);
+                    layoutRazao.setVisibility(View.GONE);
+                    layoutCPF.setVisibility(View.VISIBLE);
+                    layoutCNPJ.setVisibility(View.GONE);
                 }
             }
         });
     }
+
+
+        public void validarCadastroUsuario(){
+
+            //Recuperar textos digitados pelo usuario
+            String textoNome = campoNome.getText().toString();
+            String textoRazao = campoRazao.getText().toString();
+            String textoCPF = campoCPF.getText().toString();
+            String textoCNPJ = campoCNPJ.getText().toString();
+            String textoEndereco = campoEndereco.getText().toString();
+            String textoTelefone = campoTelefone.getText().toString();
+            String textoEmail = campoEmail.getText().toString();
+            String textoSenha = campoSenha.getText().toString();
+            String textoConfirmaSenha = campoConfirmaSenha.getText().toString();
+
+            if( !textoNome.isEmpty() ) { //verifica se o nome foi preenchido
+
+            } else {
+                Toast.makeText(CadastroActivity.this,
+                        "Preencha o nome!",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+
+
 
 
 
